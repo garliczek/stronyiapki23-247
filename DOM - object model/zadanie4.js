1f3daf21535542b09d1518b49c4b8042472132af
garliczek <114578254+garliczek@users.noreply.github.com>
2023-10-09 11:12:49 +0200

    sss

---
+++ DOM - object model/zadanie4.js
@@ -0,0 +1,6 @@
+function showText(e) {
+let = textToShow = document.querySelector("#paragraph" + e);
+textToShow.style.display = "none";
+let arrow = document.querySelector("#strzalka" + e);
+if (textToShow.style.display === "none")
+}
