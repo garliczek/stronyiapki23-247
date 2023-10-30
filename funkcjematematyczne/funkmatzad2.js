67a07ae4aab0a1004c2fb798a67e14c4ddb6bd29
garliczek <114578254+garliczek@users.noreply.github.com>
2023-10-30 10:27:08 +0100

    ddd

--- funkcjematematyczne/funkmatzad2.js
+++ funkcjematematyczne/funkmatzad2.js
@@ -1,13 +1,12 @@
 function generateRandomNumbers() {
-    let x = prompt("Podaj dolną granicę przedziału:");
-    let y = prompt("Podaj górną granicę przedziału:");
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
     let numbers = [];
     for (let i = 0; i < 10; i++) {
-      numbers.push(Math.floor(Math.random() * (y - x + 1)) + parseInt(x));
+        numbers.push(Math.floor(Math.random() * (y - x + 1)) + parseInt(x));
     }
     let randomNumbersRow = document.getElementById("randomNumbers");
     for (let i = 0; i < numbers.length; i++) {
-      randomNumbersRow.cells[i].innerHTML = numbers[i];
+        randomNumbersRow.cells[i].innerHTML = numbers[i];
     }
-  }
-  
+}
