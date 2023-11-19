1e46477ec13e9a26f6b11139d488efbf183d78b0
garliczek <114578254+garliczek@users.noreply.github.com>
2023-11-19 18:26:08 +0000

    prava domowa

---
+++ elementy_input.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.getElementById("myForm");
+
+    for (var i = 1; i <= 100; i++) {
+        var input = document.createElement("input");
+        input.type = "text";
+        input.id = "input" + i;
+        input.name = "input" + i;
+        input.placeholder = "Element numer " + i;
+        input.style.margin = "10px";
+        input.style.boxShadow = "5px 5px lightblue";
+
+
+        var columnNumber = Math.ceil(i / 20);
+
+
+        input.style.marginRight = (columnNumber < 5
+}
